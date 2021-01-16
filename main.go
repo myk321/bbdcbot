da5b650da18bf279336d4df6d1d1e9d8a2602c81
myk321 <myk321@gmail.com>
2021-01-16 10:42:13 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -17,17 +18,24 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main(){
+func main() {
 	if os.Getenv("IS_HEROKU") != "TRUE" {
-	loadEnvironmentalVariables()
+		loadEnvironmentalVariables()
 	}
 
 	//set up telegram info
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
 	errCheck(err, "Failed to start telegram bot")
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-	chatID, err := parseChatIDList((os.Getenv("CHAT_ID")))
-	errCheck(err, "Failed to fetch chat ID")
+	chatIDs, err := parseChatIDList((os.Getenv("CHAT_ID")))
+	errCheck(err, "Failed to fetch chat IDs")
+
+	client := &http.Client{}
+	jar := &myjar{} // add cookie jar so you can store the session ID cookie
+	jar.jar = make(map[string][]*http.Cookie)
+	client.Jar = jar
+
+	tgclient := AlertService{Bot: bot, ReceiverIDs: chatIDs}
 
 	//for heroku
 	go func() {
@@ -36,20 +44,14 @@ func main(){
 	}()
 	for {
 		//fetching cookies
-		log.Println("Fetching cookies")
-		sessionID := fetchCookies()
-
-		//logging in
 		log.Println("Logging in")
 		err = logIn(os.Getenv("NRIC"), os.Getenv("PASSWORD"), client)
 		errCheck(err, "Error logging in")
 
-		//fetching the booking page
-		log.Println("Fetching booking slots")
-		rawSlots, err := slotPage(os.Getenv("ACCOUNT_ID"), client, sessionID)
+		log.Println("Fetching slots")
+		rawSlots, err := slotPage(os.Getenv("ACCOUNT_ID"), client)
 		errCheck(err, "Error getting slot page")
 
-		//parse booking page to get booking dates
 		log.Println("Parsing slots")
 		slots, err := extractSlots(rawSlots)
 		errCheck(err, "Error parsing slot page")
@@ -64,7 +66,9 @@ func main(){
 		if len(valids) != 0 {
 			tgclient.MessageAll("Finished getting slots")
 		}
-		
+
+		log.Println("Finished getting slots")
+
 		//Sleep for a random duration
 		r := rand.Intn(300) + 120
 		s := fmt.Sprint(time.Duration(r) * time.Second)
@@ -72,13 +76,73 @@ func main(){
 		time.AfterFunc(30*time.Second, ping)
 		time.Sleep(time.Duration(r) * time.Second)
 	}
+
 }
 
-func loadEnvironmentalVariables() {
-	err := godotenv.Load()
+// returns slots that should be autobooked/alerted about
+func validSlots(slots []DrivingSlot) []DrivingSlot {
+	valids := make([]DrivingSlot, 0)
+	for _, slot := range slots {
+		if slot.Date.Sub(time.Now()) < 10*(24*time.Hour) { // if slot is within 10 days of now
+			if slot.Date.Sub(time.Now()) > 1*(24*time.Hour) { // if slot is more than 24 hours from now
+				if slot.SessionNumber != "1" { // slot can't be today
+					valids = append(valids, slot)
+				}
+			}
+
+		}
+	}
+	return valids
+}
+
+func book(accountID string, slot DrivingSlot, client *http.Client) error {
+	req, err := http.NewRequest("POST", "http://www.bbdc.sg/bbdc/b-2-pLessonBookingDetails.asp",
+		strings.NewReader(paymentForm(accountID, slot.SlotID).Encode()))
 	if err != nil {
-		log.Print("Error loading environmental variables: ")
-		log.Fatal(err.Error())
+		return errors.New("Error creating request: " + err.Error())
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	_, err = client.Do(req)
+	if err != nil {
+		return errors.New("Error sending request: " + err.Error())
+	}
+
+	return nil
+}
+
+type myjar struct {
+	jar map[string][]*http.Cookie
+}
+
+func (p *myjar) SetCookies(u *url.URL, cookies []*http.Cookie) {
+	fmt.Printf("The URL is : %s\n", u.String())
+	fmt.Printf("The cookie being set is : %s\n", cookies)
+	p.jar[u.Host] = cookies
+}
+
+func (p *myjar) Cookies(u *url.URL) []*http.Cookie {
+	fmt.Printf("The URL is : %s\n", u.String())
+	fmt.Printf("Cookie being returned is : %s\n", p.jar[u.Host])
+	return p.jar[u.Host]
+}
+
+// DrivingSlot represents a CDC slot to go for driving lessons
+type DrivingSlot struct {
+	SlotID        string
+	Date          time.Time
+	SessionNumber string
+}
+
+// AlertService is a service for alerting many telegram users
+type AlertService struct {
+	Bot         *tgbotapi.BotAPI
+	ReceiverIDs []int64
+}
+
+// MessageAll sends a message to all chats in the alert service
+func (as *AlertService) MessageAll(msg string) {
+	for _, chatID := range as.ReceiverIDs {
+		alert(msg, as.Bot, chatID)
 	}
 }
 
@@ -95,17 +159,10 @@ func parseChatIDList(list string) ([]int64, error) {
 	return chatIDs, nil
 }
 
-func fetchCookies() (*http.Cookie) {
-	resp, err := http.Get("http://www.bbdc.sg/bbdc/bbdc_web/newheader.asp")
-	errCheck(err, "Error fetching cookies (sessionID)")
-	sessionID := resp.Cookies()[0]
-	return sessionID
-}
-
 func logIn(nric string, pwd string, client *http.Client) error {
 	loginForm := url.Values{}
 	loginForm.Add("txtNRIC", nric)
-	loginForm.Add("txtPassword", pwd)
+	loginForm.Add("txtpassword", pwd)
 	loginForm.Add("btnLogin", "ACCESS+TO+BOOKING+SYSTEM")
 	req, err := http.NewRequest("POST", "http://www.bbdc.sg/bbdc/bbdc_web/header2.asp", strings.NewReader(loginForm.Encode()))
 	if err != nil {
@@ -118,23 +175,6 @@ func logIn(nric string, pwd string, client *http.Client) error {
 		return errors.New("Error sending request: " + err.Error())
 	}
 	return nil
-}
-
-func slotPage(accountID string, client *http.Client, sessionID string) (string, error) {
-	req, err := http.NewRequest("POST", "http://www.bbdc.sg/bbdc/b-2-pLessonBooking1.asp",
-		strings.NewReader(bookingForm(accountID).Encode()))
-	if err != nil {
-		return "", errors.New("Error creating request to get slot booking page: " + err.Error())
-	}
-	req.AddCookie(sessionID)
-	req.AddCookie(&http.Cookie{Name: "language", Value: "en-US"})
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", errors.New("Error sending request: " + err.Error())
-	}
-	body, _ := ioutil.ReadAll(resp.Body)
-	return string(body), nil
 }
 
 func extractSlots(slotPage string) ([]DrivingSlot, error) {
@@ -166,21 +206,45 @@ func extractSlots(slotPage string) ([]DrivingSlot, error) {
 	return slots, nil
 }
 
+func slotPage(accountID string, client *http.Client) (string, error) {
+	req, err := http.NewRequest("POST", "http://www.bbdc.sg/bbdc/b-2-pLessonBooking1.asp",
+		strings.NewReader(bookingForm(accountID).Encode()))
+	if err != nil {
+		return "", errors.New("Error creating request: " + err.Error())
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", errors.New("Error sending request: " + err.Error())
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	// ioutil.WriteFile("booking.txt", body, 0644)
+	return string(body), nil
+}
+
 func alert(msg string, bot *tgbotapi.BotAPI, chatID int64) {
 	telegramMsg := tgbotapi.NewMessage(chatID, msg)
 	bot.Send(telegramMsg)
 	log.Println("Sent message to " + strconv.FormatInt(chatID, 10) + ": " + msg)
 }
 
-func paymentForm(slotID string) url.Values {
+func loadEnvironmentalVariables() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Print("Error loading environmental variables: ")
+		log.Fatal(err.Error())
+	}
+}
+
+func paymentForm(accountID string, slotID string) url.Values {
 	form := url.Values{}
-	form.Add("accId", os.Getenv("ACCOUNT_ID"))
+	form.Add("accId", accountID)
 	form.Add("slot", slotID)
 
 	return form
 }
 
-func bookingForm() url.Values {
+func bookingForm(accountID string) url.Values {
 	bookingForm := url.Values{}
 	bookingForm.Add("accId", os.Getenv("ACCOUNT_ID"))
 	months := strings.Split(os.Getenv("WANTED_MONTHS"), ",")
